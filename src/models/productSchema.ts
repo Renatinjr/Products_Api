ec2aba6d073f285547ad094ac3e76e660f6007bc
Renato <renato@intranet.katuxa>
2023-05-08 11:55:33 -0300

    fixing structure ProductSchema

--- src/models/productSchema.ts
+++ src/models/productSchema.ts
@@ -3,13 +3,23 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 export const objectId = new mongoose.Types.ObjectId();
 
-export interface Products {
+export interface IProducts {
+  _id: any;
   name: string;
   price: number;
   quantity: number;
   image_url: string;
 }
 
-const ProductSchema = new Schema<Products>({});
+const ProductSchema = new Schema<IProducts>({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  image_url: { type: String, required: true },
+});
 
-export const ProductModel = mongoose.model("products", ProductSchema);
+export const ProductModel = mongoose.model(
+  "products",
+  ProductSchema,
+  "products"
+);
